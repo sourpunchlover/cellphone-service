cf689c61f13d1732afa8de7655e48b95bee2cd5f
sourpunchlover <sndgpdesigns@gmail.com>
2026-04-15 11:57:43 -0400

    Added new instance of CellPhone w/o starter data in CellPhoneApplication.

--- src/main/java/com/pluralsight/CellPhoneApplication.java
+++ src/main/java/com/pluralsight/CellPhoneApplication.java
@@ -2,6 +2,11 @@ package com.pluralsight;
 
 public class CellPhoneApplication {
     public static void main(String[] args) {
-
+    CellPhone apple = new CellPhone(0, "", "", "", "");
+        System.out.println(apple.getSerialNumber());
+        System.out.println(apple.getModel());
+        System.out.println(apple.getCarrier());
+        System.out.println(apple.getPhoneNumber());
+        System.out.println(apple.getOwner());
     }
 }
