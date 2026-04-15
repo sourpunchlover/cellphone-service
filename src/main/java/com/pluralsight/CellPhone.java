63b07aa7be3f7039c0dca02a0bce0b58e48204ba
sourpunchlover <sndgpdesigns@gmail.com>
2026-04-15 11:23:10 -0400

    Added constructor w/ no parameters & default values to CellPhone class.

--- src/main/java/com/pluralsight/CellPhone.java
+++ src/main/java/com/pluralsight/CellPhone.java
@@ -1,4 +1,18 @@
 package com.pluralsight;
 
 public class CellPhone {
+    private int serialNumber;
+    private String model;
+    private String carrier;
+    private String phoneNumber;
+    private String owner;
+
+    //Adding the constructor here
+    public CellPhone(int serialNumber, String model, String carrier, String phoneNumber, String owner){
+        this.serialNumber = 0;
+        this.model = "";
+        this.carrier = "";
+        this.phoneNumber = "";
+        this.owner = "";
+    }
 }
--- src/main/java/com/pluralsight/CellPhoneApplication.java
+++ src/main/java/com/pluralsight/CellPhoneApplication.java
@@ -1,4 +1,7 @@
 package com.pluralsight;
 
 public class CellPhoneApplication {
+    public static void main(String[] args) {
+
+    }
 }
