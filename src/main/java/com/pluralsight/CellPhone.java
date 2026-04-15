da2360e575996e95b83ee50c0a337baaafddfc14
sourpunchlover <sndgpdesigns@gmail.com>
2026-04-15 11:49:46 -0400

    Added getter & setter methods.

--- src/main/java/com/pluralsight/CellPhone.java
+++ src/main/java/com/pluralsight/CellPhone.java
@@ -7,12 +7,50 @@ public class CellPhone {
     private String phoneNumber;
     private String owner;
 
-    //Adding the constructor here
-    public CellPhone(int serialNumber, String model, String carrier, String phoneNumber, String owner){
+    //Adding the constructor here.
+    public CellPhone(int serialNumber, String model, String carrier, String phoneNumber, String owner) {
         this.serialNumber = 0;
         this.model = "";
         this.carrier = "";
         this.phoneNumber = "";
         this.owner = "";
+    }
+
+    //Adding getter and setter methods here.
+    public int getSerialNumber(){
+        return serialNumber;
+    }
+
+    public void setSerialNumber(int serialNumber) {
+        this.serialNumber = serialNumber;
+    }
+    public String getModel(){
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+    public String getCarrier(){
+        return carrier;
+    }
+
+    public void setCarrier(String carrier) {
+        this.carrier = carrier;
+    }
+    public String getPhoneNumber(){
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
     }
 }
