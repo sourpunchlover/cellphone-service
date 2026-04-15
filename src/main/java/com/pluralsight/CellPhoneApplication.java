1990f001e6ed6c1b9a0f9e47c965098478e82f63
sourpunchlover <sndgpdesigns@gmail.com>
2026-04-15 13:35:03 -0400

    Added apple.set to add the user's input values

--- src/main/java/com/pluralsight/CellPhoneApplication.java
+++ src/main/java/com/pluralsight/CellPhoneApplication.java
@@ -1,12 +1,38 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class CellPhoneApplication {
+
     public static void main(String[] args) {
-    CellPhone apple = new CellPhone(0, "", "", "", "");
-        System.out.println(apple.getSerialNumber());
-        System.out.println(apple.getModel());
-        System.out.println(apple.getCarrier());
-        System.out.println(apple.getPhoneNumber());
-        System.out.println(apple.getOwner());
+        Scanner scanner = new Scanner(System.in);
+        //Need to get user input
+        System.out.print("Enter the phone serial number: ");
+        int serialNumber = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.print("Enter the phone model: ");
+        String model = scanner.nextLine();
+
+        System.out.print("Enter the phone carrier: ");
+        String carrier = scanner.nextLine();
+
+        System.out.print("Enter the phone number: ");
+        String phoneNumber = scanner.nextLine();
+
+        System.out.print("Enter the owner: ");
+        String owner = scanner.nextLine();
+
+        CellPhone apple = new CellPhone(0, "", "", "", "");
+        //System.out.println(apple.getSerialNumber());
+        //System.out.println(apple.getModel());
+        //System.out.println(apple.getCarrier());
+        //System.out.println(apple.getPhoneNumber());
+        //System.out.println(apple.getOwner());
+        apple.setSerialNumber(serialNumber);
+        apple.setModel(model);
+        apple.setCarrier(carrier);
+        apple.setPhoneNumber(phoneNumber);
+        apple.setOwner(owner);
     }
 }
